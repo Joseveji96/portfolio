28c1eb7274635eb170cd9f2fa59f9deeed853356
Joseveji96 <joseveji96@gmail.com>
2024-12-05 20:31:50 -0600

    add the theme button feature

--- src/components/home/NavBar.tsx
+++ src/components/home/NavBar.tsx
@@ -1,35 +1,42 @@
 import { Circle } from 'lucide-react'
-import React from 'react'
+import React, { useState } from 'react'
 
 const NavBar = () => {
-  return (
-    <header className="p-0 m-0 w-full h-32 flex items-center font-dm text-textColor font-bold text-[1.5rem]">
-        <div className="w-full">
-            <nav className="relative flex justify-between px-10 ">
-                <a href="./">
-                    Eduardo Developer
-                </a>
-                <ul className="list-none flex space-x-6"> 
-                    <li><a className="" href="">What I Do</a></li>
-                    <li><a className="" href="">Projects</a></li>
-                    <li><a className="" href="">Method</a></li>
-                    <li><a className="" href="">About</a></li>
-                    <li><a className="" href="">Articles</a></li>
-                </ul>
-                <div className='flex gap-2'>
-                    <a href="">Contact Me</a>
-                    <button className="flex gap-2 justify-center items-center border-2 p-1 border-neutral-950 text-sm font-medium" 
-                        style={{borderRadius: 18}}
-                        aria-label="View Case Study">
-                            Dark
-                            <Circle width={16}/>
-                    </button>
-                </div>
-            </nav>
-        </div>
-    </header>
+    const [onDarkTheme, setOnDarkTheme] = useState(false);
+    return (
+        <header className="px-28 m-0 w-full h-32 flex items-center font-dm text-textColor font-medium text-[1.5rem]">
+            <div className="w-full">
+                <nav className="relative flex justify-between">
+                    <a href="./">
+                        Eduardo Developer
+                    </a>
+                    
+                    <div className='flex space-x-6'>
+                        <ul className="list-none flex space-x-6"> 
+                            <li><a className="" href="">Projects</a></li>
+                            <li><a className="" href="">Method</a></li>
+                            <li><a className="" href="">About</a></li>
+                            <li><a className="" href="">Articles</a></li>
+                        </ul>
+                        <a href="">Contact Me</a>
+                        <button className="flex gap-4 justify-center items-center border-2 py-[5px] px-[10px] border-black text-sm font-extrabold" 
+                            style={{borderRadius: 18}}
+                            onClick={() => setOnDarkTheme(!onDarkTheme)}
+                            aria-label="View Case Study">
+                                {
+                                    onDarkTheme ?
+                                        <h1>Dark</h1>
+                                    :
+                                        <h1>Light</h1>   
+                                }
+                                <Circle width={16} height={16}  style={ onDarkTheme ?  {backgroundColor: "black", borderRadius: 100} : {}}/>
+                        </button>
+                    </div>
+                </nav>
+            </div>
+        </header>
 
-  )
+    )
 }
 
 export default NavBar
