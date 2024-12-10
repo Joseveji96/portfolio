b85fa04eb2b73f312f0a5e34e670a856edb05580
Joseveji96 <joseveji96@gmail.com>
2024-12-09 23:56:27 -0600

    Small update to about section

---
+++ src/components/Botton/targetButtons.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const targetButtons = () => {
+  return (
+    <div>targetButtons</div>
+  )
+}
+
+export default targetButtons
--- src/components/home/Parallax.tsx
+++ src/components/home/Parallax.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { motion } from 'framer-motion';
 import React from 'react';
+import  Image from "next/image"
 
 const Parallax = () => {
   return (
@@ -20,7 +21,7 @@ const Parallax = () => {
           margin: "-200px 0px -600px 0px"
 
         }}>
-        <motion.div className="h-full w-full bg-cover bg-center p-12" 
+        <motion.div className="h-full w-full bg-cover bg-center p-12 flex justify-between" 
         style={{ backgroundImage: 'url("/imgFond.jpg")', borderRadius: 14}}
         initial={{opacity: 1}}
         whileInView={{
@@ -37,8 +38,22 @@ const Parallax = () => {
           
         }}
         >
-          <h1 className="text-8xl font-dm font-normal">About</h1>
-          <p>I'm a software engineer based in Mexico</p>
+          <div className='flex flex-col justify-between'>
+            <h1 className="text-8xl font-dm font-normal">About</h1>
+            <Image  
+								src={"/flor.svg"}
+								alt={'Flor'}
+								width={66}
+								height={64}
+								className="block 3xl:w-48"
+							/>
+          </div>
+          <div className='w-[70%]'>
+            <p>I&apos;m a software developer, specialized on web and mobile development with a focus on building innovative technology solutions. 
+              I&apos;m passionate about solving complex problems using modern technologies like JavaScript, React, Node.js, and Flutter.
+              I&apos;m always looking for ways to streamline development and improve user experience.
+            </p>
+          </div>
         </motion.div>
     </motion.div>
   );
