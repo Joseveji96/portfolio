0a9f93a71af7d1a6236a74651976f824d950470c
Joseveji96 <joseveji96@gmail.com>
2024-12-26 20:20:10 -0600

    Refactor AboutSection component with updated layout and content, enhancing user experience and visual appeal

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -1,39 +1,46 @@
 'use client'
 import { Accordion, AccordionItem } from "@nextui-org/accordion";
 import { DiamondPlus } from 'lucide-react';
+import Image from "next/image";
 
 export default function AboutSection() {
-  return (
-    <section className='flex flex-row justify-between p-12'>
-      <div className='flex flex-col'>
-        <h1 className="text-8xl font-dm font-normal">About</h1>
-        {/* <Image src={"/flor.svg"} alt={'Flor'} width={66} height={64} className="block 3xl:w-20 sticky top-[600px]"/> */}
-      </div>
-
-      <div className='max-w-[70%] font-dm'>
-        <p className='text-3xl text-textColor font-light'>I&apos;m a software developer, specialized on web and mobile development with a focus on building innovative technology solutions.
-          I&apos;m passionate about solving complex problems using modern technologies like JavaScript, React, Node.js, and Flutter.
-          I&apos;m always looking for ways to streamline development and improve user experience.
-        </p>
-        <div className='mt-10'>
-          <Accordion>
-            <AccordionItem key="anchor" aria-label="Anchor" indicator={<DiamondPlus />} startContent={"01"} title={<h1 className='text-3xl font-bold text-textColor'>Software Developer</h1>} >
-              {defaultContent}
-            </AccordionItem>
-            <AccordionItem key="full" aria-label="Full" indicator={<DiamondPlus />} startContent={"02"} title={<h1 className='text-3xl font-bold text-textColor'>Full Stack Developer</h1>} >
-              {defaultContent}
-            </AccordionItem>
-            <AccordionItem key="mobile" aria-label="Mobile" indicator={<DiamondPlus />} startContent={"03"} title={<h1 className='text-3xl font-bold text-textColor'>Mobile Developer</h1>} >
-              {defaultContent}
-            </AccordionItem>
-
-          </Accordion>
-        </div>
-      </div>
-    </section>
-  )
+	return (
+		<section className='p-12'>
+			{/* Title */}
+			<div>
+				<div>
+					<h1>Beyond</h1>
+					<h1>The c<span>o</span>de</h1>
+				</div>
+				<h3>[About Me]</h3>
+			</div>
+			{/* Content */}
+			<div>
+				<div className='mt-10'>
+					<p>I am a software developer, specialising in web and mobile development with a focus on building innovative technology solutions. I am passionate about solving complex problems. I am always looking for ways to streamline development and improve the user experience. </p>
+					<h3>[ Experience of my capabilities ]</h3>
+					<Accordion>
+						<AccordionItem key="winner" aria-label="Anchor" indicator={<DiamondPlus />} startContent={"2021"} title={<h1 className=''>Winner of the Jalisco regional stage, InnovatecNM</h1>} >
+							{/* {defaultContent} */} Hola
+						</AccordionItem>
+						<AccordionItem key="agile" aria-label="Anchor" indicator={<DiamondPlus />} startContent={"2021"} title={<h1 className=''>I completed the Certification for the Scrum Methodology</h1>} >
+							{/* {defaultContent} */} Hola
+						</AccordionItem>
+						<AccordionItem key="winnerAgain" aria-label="Anchor" indicator={<DiamondPlus />} startContent={"2021"} title={<h1 className=''>Once again winner of the Jalisco regional stage, InnovatecNM</h1>} >
+							{/* {defaultContent} */} Hola
+						</AccordionItem>
+						<AccordionItem key="article" aria-label="Anchor" indicator={<DiamondPlus />} startContent={"2021"} title={<h1 className=''>Co-Wrote an article about Machine Vision</h1>} >
+							{/* {defaultContent} */} Hola
+						</AccordionItem>
+						<AccordionItem key="graduate" aria-label="Anchor" indicator={<DiamondPlus />} startContent={"2021"} title={<h1 className=''>Graduate of ING. Computer Systems (On hold)</h1>} >
+							{/* {defaultContent} */} Hola
+						</AccordionItem>
+					</Accordion>
+				</div>
+				<div>
+					<Image src={"/fondo-VA.jpg"} width={0} height={0} alt="I"/>
+				</div>
+			</div>
+		</section>
+	)
 }
-
-const defaultContent =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-
