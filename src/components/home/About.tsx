88ee15b5ed69adfd144670c1bbf38cd03b9cb32d
Joseveji96 <joseveji96@gmail.com>
2024-12-27 22:48:39 -0600

    Add Footer component and integrate it into Home layout, enhancing site structure and user navigation

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { Accordion, AccordionItem } from "@nextui-org/accordion";
-import { DiamondPlus } from 'lucide-react';
 import Image from "next/image";
 
 export default function AboutSection() {
