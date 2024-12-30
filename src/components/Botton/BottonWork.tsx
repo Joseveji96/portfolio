5c4a6cd3e31942d42804f9995eebf65f94a8ce37
Joseveji96 <joseveji96@gmail.com>
2024-12-30 15:33:33 -0600

    Refactor BottonWork and Hero components for improved styling and responsiveness, adjusting button sizes and layout for better mobile/tablet support

--- src/components/Botton/BottonWork.tsx
+++ src/components/Botton/BottonWork.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const BottonWork = () => {
   return (
-    <button className="hidden relative sm:inline-flex h-8 md:h-10 overflow-hidden rounded-full p-[3px]">
+    <button className="hidden relative sm:inline-flex h-8 lg:h-10 overflow-hidden rounded-full p-[3px]">
         <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#446790_0%,#565364_50%,#2076A1_100%)]" />
-        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-background px-5 py-1 text-xs md:text-base font-semibold sm:font-bold text-textColor backdrop-blur-3xl">
+        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-background px-5 py-1 text-xs lg:text-base font-semibold sm:font-bold text-textColor backdrop-blur-3xl">
             Ready to work
         </span>
     </button>
--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -34,37 +34,37 @@ const Hero: React.FC<HeroProps> = ({ opacity, translateY }) => {
 	}, []);
 
 	return (
-		<section className='w-screen text-textColor  md:bg-red-400'>
+		<section className='w-screen text-textColor'>
 			<motion.div
 				style={{ opacity, translateY, transform: translateY }}
 				className='flex flex-col justify-center items-center gap-2'
 			>
-				<motion.div variants={slideUp2} initial="initial" animate="enter" className='font-dm font-bold text-5xl sm:text-6xl xl:text-8xl 2xl:text-9xl 3xl:text-[10rem] uppercase tracking-wide px-4 sm:px-5 md:px-24 3xl:px-36'>
+				<motion.div variants={slideUp2} initial="initial" animate="enter" className='font-dm font-bold text-5xl sm:text-6xl md:text-7xl xl:text-8xl 2xl:text-9xl 3xl:text-[10rem] uppercase tracking-wide px-4 sm:px-5 md:px-6 xl:px-24 3xl:px-36'>
 					<h1 className='pl-0 text-center sm:text-left'>EVERY PROJECT</h1>
-					<div className='flex flex-row justify-center items-center sm:pl-6 md:pl-28'>
+					<div className='flex flex-row justify-center items-center sm:pl-6 xl:pl-28'>
 						<h1 className='text-center sm:text-left'>Can be</h1>
-						<motion.div variants={slideUp} initial="initial" animate="enter" className="mx-4 sm:mx-6 md:mx-10 my-4 sm:my-0">
+						<motion.div variants={slideUp} initial="initial" animate="enter" className="mx-4 sm:mx-6 xl:mx-10 my-4 sm:my-0">
 							<Image
 								ref={imageRef}
 								src={"/flor.svg"}
 								alt={'Flor'}
 								width={100}
 								height={100}
-								className="block w-14 md:w-32 3xl:w-48"
+								className="block w-14 md:w-24 3xl:w-48"
 							/>
 						</motion.div>
 						<h1 className='text-center sm:text-left'>a new</h1>
 					</div>
 				</motion.div>
 				{/* Description On Desktop */}
-				<div className='px-4 md:px-40 xl:px-36 font-dm flex flex-col sm:flex-row gap-4 md:gap-20 3xl:gap-32'>
-					<div className='hidden sm:flex flex-col gap-1 pt-3 md:pl-28 text-center sm:text-right font-normal text-lg sm:text-base xl:text-2xl 3xl:text-3xl'>
+				<div className='px-4 md:px-18 xl:px-36 font-dm flex flex-col sm:flex-row gap-4 xl:gap-20 3xl:gap-32'>
+					<div className='hidden sm:flex flex-col gap-1 pt-3 xl:pl-28 text-center sm:text-right font-normal text-lg sm:text-base md:text-xl xl:text-2xl 3xl:text-3xl'>
 						<h2 className='font-bold text-base md:text-lg xl:text-xl 3xl:text-2xl bg-clip-text text-transparent sm:pl-5' style={{
 							backgroundImage: 'linear-gradient(to right, #1D1D1D, #003659)',
 							WebkitBackgroundClip: 'text',
 							WebkitTextFillColor: 'transparent',
 						}}>Hi! i´m Eduardo, A Web & Mobil developer</h2>
-						<h2 className='sm:-translate-x-10 md:-translate-x-44'>Let´s Transform <span className='font-extrabold text-[#524848]'>constraints</span> </h2>
+						<h2 className='sm:-translate-x-10 md:-translate-x-16 xl:-translate-x-44'>Let´s Transform <span className='font-extrabold text-[#524848]'>constraints</span> </h2>
 						<div>
 							<h2>into {"<"}code{"/>"} <span className="font-frank italic text-slate-50 px-3" style={{ backgroundImage: "url('/o.png')", backgroundSize: "cover", display: "inline-block", width: "auto", height: "100%", borderRadius: 10 }}>Mastepieces</span></h2>
 						</div>
@@ -131,31 +131,31 @@ const Hero: React.FC<HeroProps> = ({ opacity, translateY }) => {
 			</motion.div>
 
 			{/* Descargas */}
-			<div className="h-auto sm:h-10 flex flex-row justify-center md:justify-between sm:gap-6 items-center px-4 md:px-28 mt-10 ">
-				<div className="flex flex-row gap-4 sm:gap-6 md:gap-16 items-center">
-					<button className="font-inter text-xs md:text-[18px] font-extrabold hover:underline decoration-1 sm:decoration-2 flex gap-1 items-center">
+			<div className="h-auto sm:h-10 flex flex-row justify-center md:justify-between sm:gap-6 md:gap-4 items-center px-4 md:px-28 mt-10">
+				<div className="flex flex-row gap-4 sm:gap-6 xl:gap-16 items-center">
+					<button className="font-inter text-xs md:text-sm xl:text-[18px] font-extrabold hover:underline decoration-1 sm:decoration-2 flex gap-1 items-center">
 						Download CV
 						<Download className="w-4 h-4 sm:w-6 sm:h-6" />
 					</button>
-					<button className="font-inter text-xs md:text-[18px] font-extrabold hover:underline decoration-1 sm:decoration-2 flex gap-1 items-center">
+					<button className="font-inter text-xs md:text-sm xl:text-[18px] font-extrabold hover:underline decoration-1 sm:decoration-2 flex gap-1 items-center">
 						Descargar CV
 						<Download className="w-4 h-4 sm:w-6 sm:h-6" />
 					</button>
 				</div>
-				<div className='flex flex-row gap-8 sm:gap-20 items-center'>
+				<div className='flex flex-row gap-8 md:gap-2 lg:gap-10 items-center'>
 					<BottonWork />
 					<motion.div
 						initial={{ opacity: 0 }}
 						animate={{ opacity: 1 }}
 						transition={{ delay: 0.8, duration: 0.5 }}
-						className="mt-0 hidden md:flex justify-center"
+						className="-mt-3 hidden md:flex justify-center"
 					>
 						<motion.span
 							animate={{ y: [0, 10, 0] }}
 							transition={{ duration: 2, repeat: Infinity }}
 							className="text-lg text-textColor md:text-xl"
 						>
-							<MoveDown strokeWidth={2} size={36} className="sm:w-12 sm:h-12" />
+							<MoveDown strokeWidth={2} size={36} className="md:w-8 md:h-8" />
 						</motion.span>
 					</motion.div>
 				</div>
