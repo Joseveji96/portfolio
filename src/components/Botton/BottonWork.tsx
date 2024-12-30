a88c05d56c49ff44166cc4715107a94d2854e9e2
Joseveji96 <joseveji96@gmail.com>
2024-12-30 11:13:53 -0600

    Refactor BottonWork and Hero components for improved responsiveness and styling for mobile devices

--- src/components/Botton/BottonWork.tsx
+++ src/components/Botton/BottonWork.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const BottonWork = () => {
   return (
-    <button className="relative inline-flex h-10 overflow-hidden rounded-full p-[3px]">
+    <button className="hidden relative sm:inline-flex h-8 sm:h-10 overflow-hidden rounded-full p-[3px] sm:p-[3px]">
         <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#446790_0%,#565364_50%,#2076A1_100%)]" />
-        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-background px-5 py-1 text-base font-bold text-textColor backdrop-blur-3xl">
+        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-background px-5 py-1 text-xs sm:text-base font-semibold sm:font-bold text-textColor backdrop-blur-3xl">
             Ready to work
         </span>
     </button>
--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -39,36 +39,37 @@ const Hero: React.FC<HeroProps> = ({ opacity, translateY }) => {
 				style={{ opacity, translateY, transform: translateY }}
 				className='flex flex-col justify-center items-center gap-2'
 			>
-				<motion.div variants={slideUp2} initial="initial" animate="enter" className='font-dm font-bold text-sm sm:text-6xl xl:text-8xl 2xl:text-9xl 3xl:text-[10rem] uppercase tracking-wide px-24 3xl:px-36'>
-					<h1 className='pl-0'>EVERY PROJECT</h1>
-					<div className='flex justify-center items-center pl-28'>
-						<h1>Can be</h1>
-						<motion.div variants={slideUp} initial="initial" animate="enter" className="mx-10">
+				<motion.div variants={slideUp2} initial="initial" animate="enter" className='font-dm font-bold text-5xl sm:text-6xl xl:text-8xl 2xl:text-9xl 3xl:text-[10rem] uppercase tracking-wide px-4 sm:px-24 3xl:px-36'>
+					<h1 className='pl-0 text-center sm:text-left'>EVERY PROJECT</h1>
+					<div className='flex flex-row justify-center items-center sm:pl-28'>
+						<h1 className='text-center sm:text-left'>Can be</h1>
+						<motion.div variants={slideUp} initial="initial" animate="enter" className="mx-4 sm:mx-10 my-4 sm:my-0">
 							<Image
-								ref={imageRef} // Referencia para GSAP
+								ref={imageRef}
 								src={"/flor.svg"}
 								alt={'Flor'}
-								width={154}
-								height={154}
-								className="block 2xl:w-32 3xl:w-48"
+								width={100}
+								height={100}
+								className="block w-14 sm:w-32 3xl:w-48"
 							/>
 						</motion.div>
-						<h1>a new</h1>
+						<h1 className='text-center sm:text-left'>a new</h1>
 					</div>
 				</motion.div>
-				<div className='px-40 font-dm flex gap-20 3xl:gap-32'>
-					<div className='flex flex-col gap-1 pt-3 pl-28 text-right font-normal sm:text-xl xl:text-2xl 3xl:text-3xl'>
-						<h2 className='font-bold sm:text-lg xl:text-xl 3xl:text-2xl bg-clip-text text-transparent' style={{
-							backgroundImage: 'linear-gradient(to right, #1D1D1D, #003659)', // Gradiente de izquierda a derecha
-							WebkitBackgroundClip: 'text', // Soporte para navegadores Webkit
-							WebkitTextFillColor: 'transparent', // Texto transparente para mostrar solo el gradiente
+				{/* Description On Desktop */}
+				<div className='px-4 sm:px-40 xl:px-36 font-dm flex flex-col sm:flex-row gap-8 sm:gap-20 3xl:gap-32'>
+					<div className='hidden sm:flex flex-col gap-1 pt-3 sm:pl-28 text-center sm:text-right font-normal text-lg sm:text-xl xl:text-2xl 3xl:text-3xl'>
+						<h2 className='font-bold text-base sm:text-lg xl:text-xl 3xl:text-2xl bg-clip-text text-transparent' style={{
+							backgroundImage: 'linear-gradient(to right, #1D1D1D, #003659)',
+							WebkitBackgroundClip: 'text',
+							WebkitTextFillColor: 'transparent',
 						}}>Hi! i´m Eduardo, A Web & Mobil developer</h2>
-						<h2 className='-translate-x-44'>Let´s Transform <span className='font-extrabold text-[#524848]'>constraints</span> </h2>
+						<h2 className='sm:-translate-x-44'>Let´s Transform <span className='font-extrabold text-[#524848]'>constraints</span> </h2>
 						<div>
 							<h2>into {"<"}code{"/>"} <span className="font-frank italic text-slate-50 px-3" style={{ backgroundImage: "url('/o.png')", backgroundSize: "cover", display: "inline-block", width: "auto", height: "100%", borderRadius: 10 }}>Mastepieces</span></h2>
 						</div>
 					</div>
-					<h1 className='pt-2 font-light sm:text-6xl xl:text-7xl 2xl:text-9xl 3xl:text-9xl uppercase'>Hist
+					<h1 className='sm:pt-2 font-light text-6xl sm:text-6xl xl:text-8xl 2xl:text-9xl 3xl:text-9xl uppercase text-center sm:text-left'>Hist
 						<span className="relative inline-block">
 							{showVideo ? (
 								<motion.span
@@ -86,7 +87,6 @@ const Hero: React.FC<HeroProps> = ({ opacity, translateY }) => {
 										style={{
 											borderRadius: 60,
 											opacity: showVideo ? 1 : 0,
-											// transform: showVideo ? 'scale(1)' : 'scale(0.95)',
 											top: '50%',
 											left: '50%',
 											transform: 'translate(-50%, -50%)',
@@ -113,33 +113,48 @@ const Hero: React.FC<HeroProps> = ({ opacity, translateY }) => {
 						</span>
 
 						ry</h1>
+					{/* Description On Mobile */}
+
+					<div className='flex sm:hidden flex-col gap-1 text-center font-normal text-xl'>
+						<h2 className='font-bold text-base bg-clip-text text-transparent' style={{
+							backgroundImage: 'linear-gradient(to right, #1D1D1D, #003659)',
+							WebkitBackgroundClip: 'text',
+							WebkitTextFillColor: 'transparent',
+						}}>Hi! i´m Eduardo, A Web & Mobil developer</h2>
+						<h2>Let´s Transform <span className='font-extrabold text-[#524848]'>constraints</span> </h2>
+						<div>
+							<h2>into {"<"}code{"/>"} <span className="font-frank italic text-slate-50 px-3" style={{ backgroundImage: "url('/o.png')", backgroundSize: "cover", display: "inline-block", width: "auto", height: "100%", borderRadius: 10 }}>Mastepieces</span></h2>
+						</div>
+					</div>
 				</div>
 			</motion.div>
-			<div className="h-10 flex justify-between items-center px-28 mt-10">
-				<div className="flex gap-16 items-center">
-					<button className="font-inter text-[18px] font-extrabold hover:underline decoration-2 flex gap-2">
+
+			{/* Descargas */}
+			<div className="h-auto sm:h-10 flex flex-row justify-center sm:justify-between items-center px-4 sm:px-28 mt-10 ">
+				<div className="flex flex-row gap-4 sm:gap-16 items-center">
+					<button className="font-inter text-xs sm:text-[18px] font-extrabold hover:underline decoration-1 sm:decoration-2 flex gap-1 items-center">
 						Download CV
-						<Download />
+						<Download className="w-4 h-4 sm:w-6 sm:h-6" />
 					</button>
-					<button className="font-inter text-[18px] font-extrabold hover:underline decoration-2 flex gap-2">
+					<button className="font-inter text-xs sm:text-[18px] font-extrabold hover:underline decoration-1 sm:decoration-2 flex gap-1 items-center">
 						Descargar CV
-						<Download />
+						<Download className="w-4 h-4 sm:w-6 sm:h-6" />
 					</button>
 				</div>
-				<div className='flex gap-20'>
+				<div className='flex flex-row gap-8 sm:gap-20 items-center'>
 					<BottonWork />
 					<motion.div
 						initial={{ opacity: 0 }}
 						animate={{ opacity: 1 }}
 						transition={{ delay: 0.8, duration: 0.5 }}
-						className="mt-0 flex justify-center"
+						className="mt-0 hidden sm:flex justify-center"
 					>
 						<motion.span
 							animate={{ y: [0, 10, 0] }}
 							transition={{ duration: 2, repeat: Infinity }}
 							className="text-lg text-textColor md:text-xl"
 						>
-							<MoveDown strokeWidth={2} size={48} />
+							<MoveDown strokeWidth={2} size={36} className="sm:w-12 sm:h-12" />
 						</motion.span>
 					</motion.div>
 				</div>
